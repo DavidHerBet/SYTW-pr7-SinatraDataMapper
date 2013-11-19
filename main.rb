2c0319b67bc8409f62400f487030da87ba97a2fe
David Hernández Bethencourt <a1ddhernandez@gmail.com>
2013-11-19 12:02:13 +0000

    Parsear vista de login

--- main.rb
+++ main.rb
@@ -15,3 +15,16 @@ User.auto_migrate!
 get '/login' do
   haml :login
 end
+
+post '/login' do
+  if (params[:user][:username].empty?) || (params[:user][:password].empty?)
+    flash[:error] = "Error: The user or the password field is empty"
+    redirect to ('/login')
+  elsif User.first(:username => "#{params[:user][:username]}", :password => "#{params[:user][:password]}")
+    flash[:success] = "Login successfully"
+    redirect to ('/')
+  else
+    flash[:error] = "The user doesn't exist or the password is invalid"
+    redirect to("/login")
+  end
+end
