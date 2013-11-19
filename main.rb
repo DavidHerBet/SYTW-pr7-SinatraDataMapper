f7b925c7e990f5bddba6882a58bfa516d387c477
David Hernández Bethencourt <a1ddhernandez@gmail.com>
2013-11-19 17:49:27 +0000

    Añadir setup para el entorno de producción

--- main.rb
+++ main.rb
@@ -12,6 +12,10 @@ configure :development do
   DataMapper.setup(:default, "sqlite3://#{Dir.pwd}/development.db")
 end
 
+configure :production do
+  DataMapper.setup(:default, ENV['DATABASE_URL'])
+end
+
 User.auto_upgrade!
 
 get '/login' do
